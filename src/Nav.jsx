b9059cf4c839c90a0e09fa00e47be9634838e789
gurrudev <pawarash000@outlook.com>
2023-06-04 23:01:03 +0530

    updated

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Nav.css'
 
 import NetflixLogo from './assets/img/logo.png'
 
+const [show, handleShow] = useState(false)
+
+const transitionNavBar = () =>{
+    if(window.scrollY > 100){
+        handleShow(true)
+    }else{
+        handleShow(false)
+    }
+}
+
+useEffect(() =>{
+    window.addEventListener('scroll', transitionNavBar)
+    return () => window.removeEventListener('scroll', transitionNavBar);        
+},[])
+
 function Nav() {
     return (
         <>
-            <div className="nav">
+            <div className={`nav ${show && "nav-black"}`}>
                 <div className="nav-contents">
                     <img className='nav-logo' src={NetflixLogo} alt="" />
                    
