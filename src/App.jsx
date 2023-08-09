a4ca60d2cdc3bf2457eae5524b1f24c42d1be7bd
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-08-09 17:11:08 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import {  Route, BrowserRouter, Routes } from 'react-router-dom'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
 import { auth } from './firebase'
 import HomeScreen from '../src/screens/HomeScreen'
@@ -12,36 +12,38 @@ import ProfileScreen from './screens/ProfileScreen'
 function App() {
   const user = useSelector(selectUser);
   const dispatch = useDispatch()
-  useEffect(()=>{
-    const unsubscribe =  auth.onAuthStateChanged(userAuth => {
-      if(userAuth){
-        console.log(userAuth)
-        dispatch(login({
-          uid : userAuth.uid,
-          email : userAuth.email,
-        }))
-      }else{
-        console.log('logged out')
-        dispatch(logout)
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // console.log(userAuth)
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        )
+      } else {
+        // console.log('logged out')
+        dispatch(logout())
       }
     })
 
     return unsubscribe;
-  }, []);
+  }, [dispatch]);
   return (
     <>
       <div className="app">
         <BrowserRouter>
           <Routes>
-            {!user ?(
-              <Route exact path="/" element={<LoginScreen/>} />
+            {!user ? (
+              <Route exact path="/" element={<LoginScreen />} />
             ) : (
               <>
                 <Route exact path="/profile" element={<ProfileScreen />} />
                 <Route exact path="/" element={<HomeScreen />} />
               </>
             )}
-          </Routes>            
+          </Routes>
         </BrowserRouter>
       </div>
     </>
--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import './ProfileScreen.css'
 import Nav from '../Nav'
+import {auth} from '../firebase'
+import {selectUser} from '../features/userSlice'
+import { useSelector } from 'react-redux'
 function ProfileScreen() {
+
+  const user = useSelector(selectUser)
+  // console.log(user)
+
   return (
     <div className='profile-screen'>
         <Nav/>
+        <div className="profile-screen-body">
+          <h1>Edit Profile</h1>
+          <div className="profile-screen-info">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117" alt="" srcSet="" />
+
+            <div className="profile-screen-details">
+              <h2>{user.email}</h2>
+              <div className="profile-screen-plans">
+                <h3>Plans</h3>
+                <button className='profile-screen-signOut-btn' onClick={()=> auth.signOut()}>Sign Out</button>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
