b1be1b2a1888bdf5501a03ba54670b315584225b
Ashutosh Pawar <pawarash000@outlook.com>
2024-03-13 00:13:19 +0530

    Movie Details page updated

--- helper/firebase.js
+++ helper/firebase.js
@@ -5,15 +5,10 @@ import 'firebase/compat/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBueF2exJYrDEhkeuDc6xKOaEP4VXKqGWo",
-  
     authDomain: "neflix-30554.firebaseapp.com",
-  
     projectId: "neflix-30554",
-  
     storageBucket: "neflix-30554.appspot.com",
-  
     messagingSenderId: "35757164016",
-  
     appId: "1:35757164016:web:1b089040a2c6362eac8935"
   
 };
